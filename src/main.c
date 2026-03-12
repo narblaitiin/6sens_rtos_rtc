6571dc4499e613919d7a44778fa8a253accaea51
narblaitiin <regis.rousseau@insa-lyon.fr>
2026-03-12 15:59:36 +0100

    combined internal RTC and external DS3231 driver to have timestamp in ms

--- src/main.c
+++ src/main.c
@@ -6,43 +6,40 @@
  */
 
 //  ========== includes ====================================================================
-#include "app_rtc.h"
-#include "app_ds3231.h"
 
+#include "app_ds3231.h"
 #include <zephyr/kernel.h>
-#include <stdbool.h>
-#include <stdio.h>
+#include <zephyr/drivers/counter.h>   
+#include <zephyr/drivers/i2c.h>    
+#include <zephyr/device.h>            
+#include <zephyr/devicetree.h>        
+#include <zephyr/posix/time.h>         
+#include <zephyr/sys/timeutil.h>
 
 //  ========== defines =====================================================================
 #define STACK_SIZE 2048
 #define PRIORITY   2
 
+//  ========== globals =====================================================================
+K_SEM_DEFINE(init_done_sem, 0, 1);
+
 //  ========== interrupt sub-routine =======================================================
-bool rtc_thread_flag = true;
 void rtc_thread_func(void)
 {
-	printk("periodic sync thread started\n");
+	k_sem_take(&init_done_sem, K_FOREVER);
+    const struct device *ds3231_dev = DEVICE_DT_GET_ONE(maxim_ds3231);
 
-	//const struct device *rtc_dev = DEVICE_DT_GET(DT_NODELABEL(rtc0));
-	const struct device *ds3231_dev = DEVICE_DT_GET_ONE(maxim_ds3231);
-	while (rtc_thread_flag == true) {
-        printk("performing periodic action\n");
-    //	(void)app_rtc_periodic_sync(rtc_dev);
-		(void)app_ds3231_periodic_sync(ds3231_dev);
-        k_sleep(K_SECONDS(30));		
-	}
+    while (true) {
+        app_ds3231_periodic_sync(ds3231_dev);   // re-anchor offset
+        k_sleep(K_SECONDS(30));
+    }	
 }
 K_THREAD_DEFINE(rtc_thread_id, STACK_SIZE, rtc_thread_func, NULL, NULL, NULL, PRIORITY, 0, 0);
 
 void rtc_work_handler(struct k_work *work_rtc)
 {
-	//printk("test internal and DS3231 RTC device\n");
-
-	// uint64_t timestamp_rtc =  app_rtc_get_time();
-	// printk("timestamp in ms (RTC): %llu\n", timestamp_rtc);
-
-	uint64_t timestamp_ds3231 = app_ds3231_get_time();
-	printk("timestamp in ms (DS3231): %llu\n", timestamp_ds3231);
+	uint64_t ts = app_get_timestamp();
+    printk("timestamp (ms): %llu\n", ts);
 }
 K_WORK_DEFINE(rtc_work, rtc_work_handler);
 
@@ -55,30 +52,21 @@ K_TIMER_DEFINE(rtc_timer, rtc_timer_handler, NULL);
 // ========== main =========================================================================
 int8_t main(void)
 {
-	// initialize DS3231 RTC device via I2C (Pins: SDA -> P0.09, SCL -> P0.0)
-	const struct device *ds3231_dev = app_ds3231_init();
+	// init and set DS3231 once at boot
+    const struct device *ds3231_dev = app_ds3231_init();
     if (!ds3231_dev) {
-        printk("failed to initialize RTC device\n");
+        printk("failed to initialize DS3231\n");
         return 0;
-    } else {
-		app_ds3231_set_time(ds3231_dev, 1770372264); // set to "2024-07-19 12:00:00" UTC
-	}
+    }
+    app_ds3231_set_time(ds3231_dev, 1741773600);
 
-	// initialize on-board RTC of MDBT50Q
-	// const struct device *rtc_dev = app_rtc_init();
-    // if (!rtc_dev) {
-    //     printk("failed to initialize RTC device\n");
-    //     return 0;
-    // } else {
-	// 	app_rtc_set_time(rtc_dev, 1721050200000ULL); // e.g., for "2024-07-15 12:30:00 UTC" in ms
-	// }
+    // start nRF internal RTC counter
+    const struct device *nrf_rtc = DEVICE_DT_GET(DT_NODELABEL(rtc2));
+    counter_start(nrf_rtc);
 
-	printk("RTC DS3231 and internal RTC Example\n");
+    printk("RTC system ready\n");
 
-	// enable periodic rtc sync thread
-	rtc_thread_flag = true;
-
-	// start the timer to trigger the interrupt subroutine every 30 seconds
-	k_timer_start(&rtc_timer, K_NO_WAIT, K_MSEC(5000));
-	return 0;
+    k_sem_give(&init_done_sem);
+    k_timer_start(&rtc_timer, K_SECONDS(5), K_SECONDS(10));
+    return 0;
 }
